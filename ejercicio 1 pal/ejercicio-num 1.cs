3a36504e74228e9f97f141c6b5f348e4169abea4
haze452va <125218780+haze452va@users.noreply.github.com>
2023-02-14 19:06:33 -0600

    Add files via upload

---
+++ ejercicio 1 pal/ejercicio-num 1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_1
+{
+    internal class Program
+    {
+        static bool bandera = true;
+        static void Main(string[] args)
+        {
+            //defenir variales
+
+            const Double pi = 3.14159;
+            Double p, cm,plg,a,b;
+
+            //ingrese dos valores
+            Console.WriteLine("digite el radio mayor en milimetros");
+            a = Double.Parse(Console.ReadLine());
+            Console.WriteLine("digite el radio menor en milimetros");
+            b = Double.Parse(Console.ReadLine());
+
+            //inicio de resolucion
+            p = pi*(3 * (a + b) - Math.Sqrt((3*a+b)*(3*b+a)));
+            cm = p / 10;
+            plg = cm / 2.54;
+
+            Math.Round(p);
+            Math.Round(cm);
+            ;
+            Console.WriteLine(" el valor del perimetro en milimetros es: " + Math.Round(p,2) + "mm");
+            Console.WriteLine(" el valor de perimetro en centimetros es: " + Math.Round(cm,2) + "cm");
+            Console.WriteLine(" el valor del perimetro en pulgadas es: " + Math.Round(plg,2)+"plg");
+            Console.WriteLine();
+            Console.ReadKey();
+
+
+
+
+
+        }
+    }
+}
+
